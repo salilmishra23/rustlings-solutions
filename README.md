06f1c759db258bb5ac1ed0ebade680173c68d8b2
Salil Mishra <salildotexe@gmail.com>
2023-10-14 02:18:05 +0200

    initial progress dump

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rustlings-solutions
+
+This repo contains my solutions to [rustlings](https://github.com/rust-lang/rustlings).
---
+++ primitive_types/primitive_types3.rs
@@ -0,0 +1,17 @@
+// primitive_types3.rs
+//
+// Create an array with at least 100 elements in it where the ??? is.
+//
+// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
+// for a hint.
+
+fn main() {
+    let a: Vec<i32> = (0..101).collect();
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed")
+    }
+}
